d62101db2dfa05ce7e224c09f78b5f2be21085a7
Chris Peers <chrispeers123@hotmail.com>
2021-12-11 23:22:50 +0000

    added database file

---
+++ netmatters.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2021 at 12:21 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `netmatters`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telephone` int(30) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(1500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(100) NOT NULL,
+  `theme` varchar(100) NOT NULL,
+  `topic` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `link` varchar(150) NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `authorImage` varchar(100) NOT NULL,
+  `postDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `theme`, `topic`, `image`, `title`, `link`, `content`, `author`, `authorImage`, `postDate`) VALUES
+(1, 'blue', 'Careers', '1st-line-support.jpg', 'IT Call Handler', '1st-line-support-technician', 'Salary Ra...', 'Rob George', 'rob-george.jpg', '2021-04-30'),
+(2, 'purple', 'Careers', 'web-project-manager.jpg', 'Web Project Manager', 'web-project-manager', 'Salary Ra...', 'Tom Lancaster', 'tom-lancaster.jpg', '2021-04-23'),
+(3, 'blue', 'news', 'rob-becomes-a.jpg', 'Rob Becomes a Netmatters Director', 'news/rob-becomes-a-netmatters-director', 'Netmatters is constantly evolving and day-by-day it continues to grow in every aspect. Despit...', 'Netmatters Ltd', 'netmatters-ltd-logo.png', '2021-04-21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
