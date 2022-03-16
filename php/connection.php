880ff5be1b2719e4a72dad3d50596f72f826ba55
Chris Peers <chrispeers123@hotmail.com>
2022-03-16 13:53:53 +0000

    Updated database connection info

--- php/connection.php
+++ php/connection.php
@@ -5,7 +5,8 @@ class Connection
   public static function connect()
   {
     try{
-      $db = new PDO("mysql:host=localhost;port=3306;dbname=netmatters", "root", "");
+      // $db = new PDO("mysql:host=localhost;port=3306;dbname=chrispee_netmatters", "root", "");
+      $db = new PDO("mysql:host=localhost;port=3306;dbname=chrispee_netmatters", "chrispee_admin", "QzG.naDTMNsX");
 
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING ); 
